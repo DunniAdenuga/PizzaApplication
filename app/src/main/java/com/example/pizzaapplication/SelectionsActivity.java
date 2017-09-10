53840ad4b803ff96de85562a8a6ee06a19ca8708
DunniAdenuga <ia005@bucknell.edu>
2017-09-10 16:11:26 -0400

    Calculate Cost on the Fly

--- app/src/main/java/com/example/pizzaapplication/SelectionsActivity.java
+++ app/src/main/java/com/example/pizzaapplication/SelectionsActivity.java
@@ -1,20 +1,27 @@
 package com.example.pizzaapplication;
 
 import android.content.Intent;
+import java.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SelectionsActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.pizzaapplication.MESSAGE";
     public static final String EXTRA_MESSAGE2 = "com.example.pizzaaapplication.MESSAGE";
+    double fullCost = 10.00; //standard pizza with no toppings or crust issues
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selections);
+
+        //TextView textView = (TextView)findViewById(R.id.textView8);
+        //String message = String.format("%.2f", 10);
+        //textView.setText(message);
 
     }
 
@@ -26,7 +33,7 @@ public class SelectionsActivity extends AppCompatActivity {
         String message = editText.getText().toString(); //gets Customer's name
         message = message + "!";
 
-        double fullCost = 10.00; //standard pizza with no toppings or crust issues
+        double fullcost = 10.00; //standard pizza with no toppings or crust issues
         CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);//onion topping
         CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkBox2);//chicken topping
         CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkBox3);//sauce
@@ -35,24 +42,101 @@ public class SelectionsActivity extends AppCompatActivity {
         CheckBox checkBox6 = (CheckBox) findViewById(R.id.checkBox6);//normal
 
         if(checkBox.isChecked())
-            fullCost = fullCost + 1.25;
+            fullcost = fullcost + 1.25;
         if(checkBox2.isChecked())
-            fullCost = fullCost + 2.00;
+            fullcost = fullcost + 2.00;
         if(checkBox3.isChecked())
-            fullCost = fullCost + 1.25;
+            fullcost = fullcost + 1.25;
 
         if(checkBox4.isChecked())
-            fullCost = fullCost + 2.25;
+            fullcost = fullcost + 2.25;
         else if(checkBox5.isChecked())
-            fullCost = fullCost +2.25;
+            fullcost = fullcost +2.25;
         else if(checkBox6.isChecked())
-            fullCost = fullCost + 1.00;
+            fullcost = fullcost + 1.00;
 
 
-        String message2 = "$ " + String.valueOf(fullCost) +"0";
+        String message2 = "$ " + String.format("%.2f", fullcost);
 
         intent.putExtra(EXTRA_MESSAGE, message);
         intent.putExtra(EXTRA_MESSAGE2, message2);
         startActivity(intent);
+    }
+
+    //Gets called if checked
+    public void onCheckClicked(View view){
+
+        TextView textView = (TextView)findViewById(R.id.textView8);
+
+        //String message = String.format("%.2f", fullCost);
+        String message = "";
+
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkBox:
+                if (checked) {
+                    fullCost = fullCost + 1.25;
+                    message = String.format("%.2f", fullCost);
+                    textView.setText(message);
+                }else{
+                    fullCost = fullCost -  1.25;
+                    message = String.format("%.2f", fullCost);
+                    textView.setText(message);}
+                break;
+            case R.id.checkBox2:
+                if (checked) {
+                    fullCost = fullCost + 2.00;
+                    message = String.format("%.2f", fullCost);
+                    textView.setText(message);
+                }else{
+                    fullCost = fullCost -  2.00;
+                    message = String.format("%.2f", fullCost);
+                    textView.setText(message);}
+                    break;
+            case R.id.checkBox3:
+                if (checked) {
+                    fullCost = fullCost + 1.25;
+                    message = String.format("%.2f", fullCost);
+                    textView.setText(message);
+                }else{
+                    fullCost = fullCost -  1.25;
+                message = String.format("%.2f", fullCost);
+                textView.setText(message);}
+                break;
+            case R.id.checkBox4:
+                if (checked) {
+                    fullCost = fullCost + 2.25;
+                    message = String.format("%.2f", fullCost);
+                    textView.setText(message);
+                }else{
+                    fullCost = fullCost -  2.25;
+                    message = String.format("%.2f", fullCost);
+                    textView.setText(message);}
+                break;
+            case R.id.checkBox5:
+                if (checked) {
+                    fullCost = fullCost + 2.25;
+                    message = String.format("%.2f", fullCost);
+                    textView.setText(message);
+                }else{
+                    fullCost = fullCost -  2.25;
+                message = String.format("%.2f", fullCost);
+                textView.setText(message);}
+                break;
+            case R.id.checkBox6:
+                if (checked) {
+                    fullCost = fullCost + 1.00;
+                    message = String.format("%.2f", fullCost);
+                    textView.setText(message);
+                }else{
+                    fullCost = fullCost -  1.00;
+                message = String.format("%.2f", fullCost);
+                textView.setText(message);}
+                break;
+        }
+
     }
 }
